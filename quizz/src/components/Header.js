c7da64c0ff57967a5ddb1879c5974831d66f8cf1
SmithankumarR <smithankumar404@gmail.com>
2022-12-22 00:18:43 +0530

    header

--- quizz/src/components/Header.js
+++ quizz/src/components/Header.js
@@ -1,21 +1,12 @@
-import { Link } from "react-router-dom";
-
+import {Link} from "react-router-dom"
 function Header(props) {
     return (
         <header className="relative">
-            < Link to = "/" >
+            < Link to="/">
                 <h1 className="z-10 absolute left-16 top-12 text-4xl font-bold text-white">Quiz App</h1>
             </Link>
-            <div className="flex justify-center">
-                <img 
-                src="./images/logo.png"
-                alt="logo"
-                width="300"
-                />
-                <h1 className=" mt-28 text-4xl  font-bold  text-red-400 mr-24">
-                     Quiz App
-                </h1>
-                
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200"><path fill="#0099ff" fillOpacity="1" d="M0,160L40,149.3C80,139,160,117,240,122.7C320,128,400,160,480,170.7C560,181,640,171,720,160C800,149,880,139,960,117.3C1040,96,1120,64,1200,58.7C1280,53,1360,75,1400,85.3L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
             </div>
         </header>
     )
